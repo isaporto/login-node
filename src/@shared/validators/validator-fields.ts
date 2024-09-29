e993b0710d2bd4be71f4521900e1b8b0d267e81a
Isabela Porto <isamporto@hotmail.com>
2024-09-29 18:19:51 +0000

    feat: :sparkles: Install new dependencie for property validation
    new npm package class-validator from typestack for entity property validations

---
+++ src/@shared/validators/validator-fields.ts
@@ -0,0 +1,22 @@
+import { validateSync } from "class-validator";
+import ValidatorFieldsInterface, { FieldsErrors } from "./validator-fields-interface";
+
+export default class ValidatorFields<ValidatedProps> implements ValidatorFieldsInterface<ValidatedProps> {
+  errors: FieldsErrors;
+  validatedData: ValidatedProps;
+
+  validate(data: any): boolean {
+    const errors = validateSync(data)
+
+    if (errors.length > 0) {
+      this.errors = {}
+      for (const error of errors) {
+        const field = error.property;
+        this.errors[field] = Object.values(error.constraints)
+      }
+    } else {
+      this.validatedData = data
+    }
+    return ! errors.length
+  }
+}
---
+++ src/user/domain/validators/user.validator.ts
@@ -0,0 +1,5 @@
+import ValidatorFields from "../../../@shared/validators/validator-fields";
+
+export class UserRules {}
+
+export class UserValidator extends ValidatorFields<UserRules> {}
