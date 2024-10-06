2e8da00d23de558e4ff81047f95d7b2b4883b048
Isabela Porto <isamporto@hotmail.com>
2024-10-06 01:32:55 +0000

    feat: :sparkles: new SearchResult class and its tests

--- src/@shared/domain/repositories/__test__/repository-contracts.spec.ts
+++ src/@shared/domain/repositories/__test__/repository-contracts.spec.ts
@@ -1,4 +1,4 @@
-import { SearchParams } from "../repository-contracts"
+import { SearchParams, SearchResult } from "../repository-contracts"
 
 describe("SearchParams Unit Tests", () => {
   function assertPropSameExpected(
@@ -105,5 +105,77 @@ describe("SearchParams Unit Tests", () => {
       params = new SearchParams({ filter: i.received })
       expect(params.filter).toBe(i.expected);
     })
+  })
+})
+
+describe("SearchResult Unit Tests", () => {
+  test("constructor props", () => {
+    let result = new SearchResult({
+      items: ['entity1', 'entity2'] as any,
+      total: 4,
+      current_page: 1,
+      per_page: 2,
+      sort: null,
+      sort_dir: null,
+      filter: null
+    })
+
+    expect(result.toJSON()).toStrictEqual({
+      items: ['entity1', 'entity2'],
+      total: 4,
+      current_page: 1,
+      per_page: 2,
+      last_page: 2,
+      sort: null,
+      sort_dir: null,
+      filter: null
+    })
+
+    result = new SearchResult({
+      items: ['entity1', 'entity2'] as any,
+      total: 4,
+      current_page: 1,
+      per_page: 2,
+      sort: 'name',
+      sort_dir: 'asc',
+      filter: "test"
+    })
+
+    expect(result.toJSON()).toStrictEqual({
+      items: ['entity1', 'entity2'],
+      total: 4,
+      current_page: 1,
+      per_page: 2,
+      last_page: 2,
+      sort: 'name',
+      sort_dir: 'asc',
+      filter: "test"
+    })
+  })
+
+  it('should set last_page 1 when per_page is greater than total field', () => {
+    const result = new SearchResult({
+      items: ['entity1', 'entity2'] as any,
+      total: 4,
+      current_page: 1,
+      per_page: 10,
+      sort: 'name',
+      sort_dir: 'asc',
+      filter: "test"
+    })
+    expect(result.last_page).toBe(1)
+  })
+
+  test('last_page prop when total is not multiple of per_page', () => {
+    const result = new SearchResult({
+      items: ['entity1', 'entity2'] as any,
+      total: 15,
+      current_page: 1,
+      per_page: 2,
+      sort: 'name',
+      sort_dir: 'asc',
+      filter: "test"
+    })
+    expect(result.last_page).toBe(8)
   })
 })
